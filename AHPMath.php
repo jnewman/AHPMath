c80776c62b56177fa47dfbc318d1f011e9c8effe
Josh Newman <joshua.l.newman@gmail.com>
2011-11-10 20:08:15 -0600

    Update AHPMath.php

--- AHPMath.php
+++ AHPMath.php
@@ -1,8 +1,5 @@
 <?php
 /**
- * Copyright (c) 2011, Monterey Institute of International Studies
- * All rights reserved.
- *
  * @license BSD type http://www.opensource.org/licenses/bsd-license.php
  * @author Joshua Newman
  *
